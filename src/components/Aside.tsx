92b5b1e812df6545df8539ae7c0da6ee20b2a2b9
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-05 19:11:03 +0200

    update aside bar styles

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -25,10 +25,10 @@ export default function Aside() {
         </nav>
       </div>
 
-      <div className='text-white'>
+      <div className='text-white mt-4 flex flex-col gap-4'>
         {notes.map((note, index) => (
-          <div key={index} className='flex justify-center items-center py-4 px-2 dark:bg-gray-950 rounded-b-lg'>
-            <Link to={`/${note.id}`} className='hover:underline'>{note.title}</Link>
+          <div key={index} className='flex justify-center items-center py-4 px-2 dark:bg-gray-950 rounded-lg text-balance text-center'>
+            <Link to={`/${note.id}`} className='hover:underline text-balance text-center'>{note.title}</Link>
           </div>
         ))}
       </div>
