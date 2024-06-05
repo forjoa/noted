b26fb763cf50d71ce7a83e3a8924ab0e498cf47f
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-06-05 17:52:44 +0200

    create personalized hook to use local storage to save notes

---
+++ src/utils/useLocalStorage.ts
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+
+interface Note {
+  id: string
+  title: string
+  content: string
+  timestamp: Date
+}
+
+const getStoredNotes = () => {
+  const storedNotes = localStorage.getItem('notes')
+  return storedNotes ? JSON.parse(storedNotes) : []
+}
+
+const useLocalStorageNotes = () => {
+  const [notes, setNotes] = useState<Note[]>(getStoredNotes)
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes))
+  }, [notes])
+
+  const addNote = (title: string, content: string, id?: string) => {
+    const timestamp = new Date().toISOString()
+    if (id) {
+      setNotes((prevNotes) =>
+        prevNotes.map((note) =>
+          note.id === id
+            ? ({ ...note, title, content, timestamp } as unknown as Note)
+            : note
+        )
+      )
+    } else {
+      const newNote: Note = {
+        id: uuidv4(),
+        title,
+        content,
+        timestamp: new Date(timestamp),
+      }
+      setNotes((prevNotes) => [...prevNotes, newNote])
+    }
+  }
+
+  return [notes, addNote] as const
+}
+
+export default useLocalStorageNotes
